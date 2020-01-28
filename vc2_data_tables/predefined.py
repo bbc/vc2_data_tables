ae436805dd205675fc4ce067b563ea22caa8c4fc
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2020-01-28 09:24:29 +0000

    Remove accidental empty file.

--- vc2_data_tables/predefined.py
+++
@@ -1,8 +0,0 @@
-"""
-Constants and Predefined Data Tables
-====================================
-
-This module defines or loads all of the constants and predefined data tables
-included in the VC-2 specification documents themselves.
-
-"""
