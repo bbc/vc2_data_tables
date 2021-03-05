0c8088d64399460cfe00a1169e470146430e2113
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2021-03-05 11:58:54 +0000

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -26,9 +26,12 @@ setup(
     long_description_content_type='text/markdown',
     license="GPL-3.0-only",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
 
         "Intended Audience :: Developers",
+        "Intended Audience :: Manufacturing",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Telecommunications Industry",
 
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
 
@@ -37,7 +40,12 @@ setup(
         "Operating System :: MacOS",
 
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="vc2 dirac dirac-pro constants data-tables quantisation-matrix",
     install_requires=["enum34; python_version < '3.4'"],
